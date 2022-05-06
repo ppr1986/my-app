5c7e6b18f9f8f5c5635cba7054ea5673b41c097b
ppr1986 <ppry1986@gmail.com>
2022-05-05 23:39:26 -0400

    javascript expressions in JSX

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM  from "react-dom";
 
+const name= "Pedro";
+const lname= "Enrique";
+
+
 ReactDOM.render(
   <div>
-    <h1>My favourite fruiets</h1>
-    <ul>
-      <li>apple</li>
-      <li>peach</li>
-      <li>pinaple</li>
-    </ul>
+    <h1>Hello! {name + " "+ lname}</h1>
+    <p>My looki number is {Math.floor(Math.random()*10)}</p>
+    
   </div>,document.getElementById("root")
 );
