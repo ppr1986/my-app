7dd89bd0e079db5793c22554b6acba3bb1d7081e
ppr1986 <ppry1986@gmail.com>
2022-05-06 00:23:16 -0400

    javascript expressions in JSX complete

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDom from "react-dom";
 
-const name= "Pedro";
-const lname= "Enrique";
+const name ="pedro";
+const curretDate = new Date();
+const year = curretDate.getFullYear();
+ReactDom.render(<div>
+  <p>Created by {name}</p>
+  <p>Copyrigth {year}</p>
+</div>, document.getElementById("root"));
 
-
-ReactDOM.render(
-  <div>
-    <h1>Hello! {name + " "+ lname}</h1>
-    <p>My looki number is {Math.floor(Math.random()*10)}</p>
-    
-  </div>,document.getElementById("root")
-);
