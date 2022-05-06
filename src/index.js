ac459121602bc1211a07bff28b1a31d6d5c31054
ppr1986 <ppry1986@gmail.com>
2022-05-05 23:15:48 -0400

    my favourites fruits

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favourite fruiets</h1>
+    <ul>
+      <li>apple</li>
+      <li>peach</li>
+      <li>pinaple</li>
+    </ul>
+  </div>,document.getElementById("root")
+);
